888536ce3116a31bbfb87f7005143fa3fe93c524
Sahid Reza <sahidreza976@gmail.com>
2026-07-28 10:25:50 +0530

    Add Edge class

--- src/main/java/org/example/BFS.java
+++ src/main/java/org/example/BFS.java
@@ -1,6 +1,14 @@
 package org.example;
 
 public class BFS {
+    public static class Edge{
+        int src;
+        int dest;
+        public Edge(int s,int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
     public static void main(String[] args) {
 
     }

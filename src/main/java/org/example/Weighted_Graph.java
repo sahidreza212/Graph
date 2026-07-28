9c0ba62d2b9df215c3d389ac13d6addfdd93f51f
Sahid Reza <sahidreza976@gmail.com>
2026-07-28 10:13:58 +0530

    Add create Graph function

--- src/main/java/org/example/Weighted_Graph.java
+++ src/main/java/org/example/Weighted_Graph.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Weighted_Graph {
     public static class Edge{
         int src;
@@ -12,8 +14,25 @@ public class Weighted_Graph {
         }
     }
 
-    public static void createGraph()
+    public static void createGraph(ArrayList<Edge>graph[]){
+        for (int i = 0;i<graph.length;i++){
+            graph[i] = new ArrayList<Edge>();
+        }
+        graph[0].add(new Edge(0,2,2));
+
+        graph[1].add(new Edge(1,2,10));
+        graph[1].add(new Edge(1,3,0));
+
+        graph[2].add(new Edge(2,0,2));
+        graph[2].add(new Edge(2,1,10));
+        graph[2].add(new Edge(2,3,-1));
+
+        graph[3].add(new Edge(3,1,0));
+        graph[3].add(new Edge(3,2,-1));
+    }
     public static void main(String[] args) {
 
+        int V = 4;
+        ArrayList<Edge>graph[] = new ArrayList[V];
     }
 }

420e6f4a4a02b12b295bde118d6bd94906520bb5
Sahid Reza <sahidreza976@gmail.com>
2026-07-29 12:02:23 +0530

    Add create graph function

--- src/main/java/org/example/Topological_Sorting.java
+++ src/main/java/org/example/Topological_Sorting.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Topological_Sorting {
     public static class Edge{
         int src;
@@ -10,6 +12,20 @@ public class Topological_Sorting {
         }
     }
 
+    public static void createGraph(ArrayList<Cycle_Detection.Edge> graph[]){
+        for (int i = 0;i<graph.length;i++){
+            graph[i] = new ArrayList<>();
+        }
+        graph[1].add(new Cycle_Detection.Edge(1,2));
+
+        graph[2].add(new Cycle_Detection.Edge(2,3));
+        graph[2].add(new Cycle_Detection.Edge(2,4));
+
+        graph[3].add(new Cycle_Detection.Edge(3,5));
+
+        graph[4].add(new Cycle_Detection.Edge(4,5));
+
+    }
     public static void main(String[] args) {
 
     }

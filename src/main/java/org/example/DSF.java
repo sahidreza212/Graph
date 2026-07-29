30a33fcd3d50cfa560c9320f56c24c843caadaf9
Sahid Reza <sahidreza976@gmail.com>
2026-07-29 09:55:02 +0530

    Add create graph functiion

--- src/main/java/org/example/DSF.java
+++ src/main/java/org/example/DSF.java
@@ -1,7 +1,48 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class DSF {
+    public static class Edge{
+        int src;
+        int dest;
+        public Edge(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+    public static void createGraph(ArrayList<Edge>graph[]){
+        for (int i = 0;i<graph.length;i++){
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new DSF.Edge(0,1));
+        graph[0].add(new DSF.Edge(0,2));
+
+        graph[1].add(new DSF.Edge(1,0));
+        graph[1].add(new DSF.Edge(1,3));
+
+        graph[2].add(new DSF.Edge(2,0));
+        graph[2].add(new DSF.Edge(2,4));
+
+        graph[3].add(new DSF.Edge(3,1));
+        graph[3].add(new DSF.Edge(3,4));
+        graph[3].add(new DSF.Edge(3,5));
+
+        graph[4].add(new DSF.Edge(4,2));
+        graph[4].add(new DSF.Edge(4,3));
+        graph[4].add(new DSF.Edge(4,5));
+
+        graph[5].add(new DSF.Edge(5,3));
+        graph[5].add(new DSF.Edge(5,4));
+        graph[5].add(new DSF.Edge(5,6));
+
+        graph[6].add(new DSF.Edge(6,5));
+    }
+
     public static void main(String[] args) {
 
+        int V = 7;
+        ArrayList<Edge>graph[] = new ArrayList[V];
+        createGraph(graph);
     }
 }

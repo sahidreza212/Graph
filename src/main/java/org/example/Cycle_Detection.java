dad22801ab6034033e0c39fb267dfedd0070d3b6
Sahid Reza <sahidreza976@gmail.com>
2026-07-29 11:25:02 +0530

    Add create function

--- src/main/java/org/example/Cycle_Detection.java
+++ src/main/java/org/example/Cycle_Detection.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Cycle_Detection {
+    public static class Edge{
+        int src;
+        int dest;
+        public Edge(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+    public static void createGraph(ArrayList<Edge>graph[]){
+        for (int i = 0;i<graph.length;i++){
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0,2));
+
+        graph[1].add(new Edge(1,0));
+
+        graph[2].add(new Edge(2,3));
+
+        graph[3].add(new Edge(3,0));
+
+    }
+
+
     public static void main(String[] args) {
 
+        int V = 4;
+        ArrayList<Edge>graph[] = new ArrayList[V];
+        createGraph(graph);
     }
 }

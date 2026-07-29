eaa54e00dfc9c553fd0afb002cb01f52818b7ba9
Sahid Reza <sahidreza976@gmail.com>
2026-07-29 12:00:03 +0530

    Add Edge

--- src/main/java/org/example/Topological_Sorting.java
+++ src/main/java/org/example/Topological_Sorting.java
@@ -1,6 +1,15 @@
 package org.example;
 
 public class Topological_Sorting {
+    public static class Edge{
+        int src;
+        int dest;
+        public Edge(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
     public static void main(String[] args) {
 
     }

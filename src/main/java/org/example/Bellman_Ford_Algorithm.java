8c4fa827f7445a569733ad88f4eb529869a46d7c
Sahid Reza <sahidreza976@gmail.com>
2026-07-30 11:22:00 +0530

    add create graph function

--- src/main/java/org/example/Bellman_Ford_Algorithm.java
+++ src/main/java/org/example/Bellman_Ford_Algorithm.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Bellman_Ford_Algorithm {
 
     public static class Edge{
@@ -11,6 +13,21 @@ public class Bellman_Ford_Algorithm {
             this.dest = d;
             this.wt = w;
         }
+    }
+    public static void createGraph(ArrayList<Edge> graph[]){
+        for(int i = 0;i<graph.length;i++){
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0,1,2));
+        graph[0].add(new Edge(0,2,4));
+
+        graph[1].add(new Edge(1,2,-4));
+
+        graph[2].add(new Edge(2,3,2));
+
+        graph[3].add(new Edge(3,4,4));
+
+        graph[4].add(new Edge(4,1,-1));
     }
     public static void main(String[] args) {
 

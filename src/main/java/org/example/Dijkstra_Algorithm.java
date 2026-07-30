50a73a86616d8a70df56b915068fabce63cc824f
Sahid Reza <sahidreza976@gmail.com>
2026-07-30 10:43:05 +0530

    Add Edge class

--- src/main/java/org/example/Dijkstra_Algorithm.java
+++ src/main/java/org/example/Dijkstra_Algorithm.java
@@ -1,6 +1,16 @@
 package org.example;
 
 public class Dijkstra_Algorithm {
+    public static class Edge{
+        int src;
+        int dest;
+        int wt;
+        public Edge(int s,int d,int w){
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
     public static void main(String[] args) {
 
     }

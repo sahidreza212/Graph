f3f238d4a028fa7cd3ce55fad43aabfee1fb3eb9
Sahid Reza <sahidreza976@gmail.com>
2026-07-30 11:18:14 +0530

    add Edge class

--- src/main/java/org/example/Bellman_Ford_Algorithm.java
+++ src/main/java/org/example/Bellman_Ford_Algorithm.java
@@ -1,6 +1,17 @@
 package org.example;
 
 public class Bellman_Ford_Algorithm {
+
+    public static class Edge{
+        int src;
+        int dest;
+        int wt;
+        public Edge(int s,int d,int w){
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
     public static void main(String[] args) {
 
     }

465c4217082d957396e3da62e801443655010116
Sahid Reza <sahidreza976@gmail.com>
2026-08-01 11:42:37 +0530

    Add create graph function

--- src/main/java/org/example/Kosaraju_Algorithm.java
+++ src/main/java/org/example/Kosaraju_Algorithm.java
@@ -1,7 +1,33 @@
 package org.example;
 
-public class Kosaraju_Algorithm {
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+public class Kosaraju_Algorithm {
+    public static class Edge{
+        int src;
+        int dest;
+
+        public Edge(int s,int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+    public static void createGraph(ArrayList<Edge> graph[]){
+        for (int i = 0;i< graph.length;i++){
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0,2));
+        graph[0].add(new Edge(0,3));
+
+        graph[1].add(new Edge(1,0));
+
+        graph[2].add(new Edge(2,1));
+
+        graph[3].add(new Edge(3,4));
+    }
+    public static void main(String[] args) {
+      int V = 5;
+      ArrayList<Edge>graph[] = new ArrayList[V];
+      createGraph(graph);
     }
 }

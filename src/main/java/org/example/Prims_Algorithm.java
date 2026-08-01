b36ea2f06a3609ff6a7188b06490ef5dcfb19740
Sahid Reza <sahidreza976@gmail.com>
2026-08-01 11:12:57 +0530

    Add Edge class

--- src/main/java/org/example/Prims_Algorithm.java
+++ src/main/java/org/example/Prims_Algorithm.java
@@ -1,6 +1,23 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Prims_Algorithm {
+    public static class Edge{
+        int src;
+        int dest;
+
+        public Edge(int s,int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>graph[]){
+        for (int i = 0;i< graph.length;i++){
+            graph[i] = new ArrayList<>();
+        }
+    }
     public static void main(String[] args) {
 
     }

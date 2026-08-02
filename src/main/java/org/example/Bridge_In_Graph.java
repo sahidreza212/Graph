f42c974499f66f3232c7828d58789d395e515ca2
Sahid Reza <sahidreza976@gmail.com>
2026-08-02 11:35:55 +0530

    add create graph function

--- src/main/java/org/example/Bridge_In_Graph.java
+++ src/main/java/org/example/Bridge_In_Graph.java
@@ -1,7 +1,41 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Bridge_In_Graph {
+    public static class Edge{
+        int src;
+        int dest;
+
+        public Edge(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>graph[]){
+        for(int i = 0; i< graph.length;i++){
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0,1));
+        graph[0].add(new Edge(0,2));
+
+        graph[1].add(new Edge(1,0));
+        graph[1].add(new Edge(1,2));
+        graph[1].add(new Edge(1,3));
+
+        graph[2].add(new Edge(2,1));
+        graph[2].add(new Edge(2,0));
+
+        graph[3].add(new Edge(3,1));
+        graph[3].add(new Edge(3,4));
+
+        graph[4].add(new Edge(4,3));
+    }
     public static void main(String[] args) {
 
+        int V = 5;
+        ArrayList<Edge>graph[] = new ArrayList[V];
+        createGraph(graph);
     }
 }
